231957b4edebc7f15254ace12f37f3865f13e5df
Vagner <vagner@odig.net>
2020-06-23 11:43:32 -0300

    chore(): create project

---
+++ src/popup/router/pages/Index.vue
@@ -0,0 +1,18 @@
+<template>
+  <p>Hello world!</p>
+  
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+p {
+  font-size: 20px;
+}
+</style>
